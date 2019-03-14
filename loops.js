8f86fbb9715e7bd182cf970e726c31e883bea61c
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-14 09:58:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,3 +14,15 @@ function whileLoop(n){
   }
   return "done";
 }
+function doWhileLoop(num){
+  var i = 0;
+ 
+  function incrementVariable() {
+      i = i + 1;
+      return i;
+  }
+ 
+  do {
+      console.log("doo-bee-doo-bee-doo");
+  } while (incrementVariable() < 5);
+}
